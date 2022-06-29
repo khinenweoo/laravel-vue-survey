710bec9151047d3978b8d896ddebf19d9c4ae098
khinenweoo <devkhinenweoo@gmail.com>
2022-06-29 20:38:21 +0630

    Login and Dashboard Auth Redirect, Reusable PageComponent

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,11 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { mapState } from 'vuex'
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   components: {
-    HelloWorld
+    
   },
   computed: {
     ...mapState(['user'])
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,8 @@ import Surveys from '../views/Surveys.vue'
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Layout from "../components/Layout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import store from "../store";
 
 const routes = [
     {
@@ -14,25 +16,40 @@ const routes = [
     {
         path: '/dashboard',
         component: Layout,
+        meta: {requiresAuth: true},
         children: [
             {path: '/dashboard', name: 'Dashboard', component: Dashboard},
             {path: '/surveys', name: 'Surveys', component: Surveys}
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true},
+        children: [
+            {path: '/login', name: 'Login', component: Login},
+            {path: '/register', name: 'Register', component: Register},
+        ]
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
 ]
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'Login'})
+    }else if(store.state.user.token && (to.meta.isGuest)) {
+        //if user is authorized and try to access login, we need to prevent this and redirect to dashboard
+        // if user has token and the route trying to access is Login or Register, redirect to dashboard
+        next({name: 'Dashboard'});
+    } else {
+        // Otherwise let the user go on the page she was trying to access
+        next()
+    }
 })
 
 export default router;
